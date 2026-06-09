bea2fb07734b2fcbdfbcd8675e1134b12e932a2e
“unaisLearning” <unais.learning@gmail.com>
2026-06-09 21:41:58 +0530

    Parametrize test added

--- tests/test_user_parametrize.py
+++ tests/test_user_parametrize.py
@@ -3,12 +3,16 @@ from utils.validator import validate_json
 import allure
 import pytest
 
-@allure.title("Successfully fetched user")
+@allure.title("Validate Multiple Users")
+@pytest.mark.parametrize(
+    "user_id",
+    [1, 2, 3]
+)
 
-def test_get_user(client):
+def test_get_multiple_users(client,user_id):
     logger = get_logger(__name__)
-    logger.info("Starting test_get_user")
-    response = client.get_single_user(2)
+    logger.info(f"Starting test for user_id={user_id}")
+    response = client.get_single_user(user_id)
 
     allure.attach(
         response.text,
@@ -19,14 +23,14 @@ def test_get_user(client):
     body = response.json()
 
     assert response.status_code == 200
-    assert body["data"]["id"] == 2
+    assert body["data"]["id"] == user_id
     assert body ["data"]["email"] is not None
-    assert body ["data"]["first_name"] == "Janet"
+
     assert response.elapsed.total_seconds() < 3
 
     logger.info("✓ Test passed")
 
     validate_json(
         "schemas/user_schema.json",
-        response.json()
+        body
     )
